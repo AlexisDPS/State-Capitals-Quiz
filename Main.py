fef13956b22b02f2907632afacd050bda8c0bf5b
AlexisDPS <depaza612@gmail.com>
2026-09-14 12:50:22 -0700

    created functions

---
+++ Main.py
@@ -0,0 +1,10 @@
+# Group 10
+# Broden Black
+# Alexis De Paz Salazar
+# Lab Assignment 4 - A program that quizzes the user on the state capitals
+
+def read_file_to_dict(file_name):
+    """
+    Reads a file
+
+def main()

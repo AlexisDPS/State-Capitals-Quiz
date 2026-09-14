df011db3704bc0123253a30df2df0707ba579df6
AlexisDPS <depaza612@gmail.com>
2026-09-14 12:51:39 -0700

    function docstrings

--- Main.py
+++ Main.py
@@ -4,7 +4,21 @@
 # Lab Assignment 4 - A program that quizzes the user on the state capitals
 
 def read_file_to_dict(file_name):
-    """
-    Reads a file
+    """Read in each line, separate the state and the capital, and store them as a key:
+    value pair in a dictionary. Return the filled dictionary."""
+    return file_name
 
-def main()
+def get_random_state(states):
+    return states
+
+def get_random_choices(states, correct_state):
+    return states
+
+def get_random_state():
+    return "hi bast"
+
+def ask_question(correct_state, possible_answers):
+    return possible_answers
+
+def main():
+    print("Welcome to the State Capitals Quiz!")
